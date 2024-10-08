dc3304f2ce44c98604a9f34515cbe608f9869289
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-10-08 16:20:00 +0530

    Add files via upload

---
+++ Basic operators.py
@@ -0,0 +1,18 @@
+#Author:Joswin Shaju
+#Date:08/10/2024
+
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+num3=int(input("enter the number:"))
+sum=num1+num2#addition
+print("The sum of ",num1," and ",num2," is:",sum)
+diff=num2-num1#subtraction
+print("The difference when ",num2," is subtracted from ",num1," is :",diff)
+prdt=num1*num2#multiplication
+print("The product of ",num1," and ",num2," is:",prdt)
+divd=num1/num2#division
+print("The quotient when ",num1," is divided by ",num2," is:",divd)
+rmnd=num1%num2#modulus
+print("The remainder when ",num1," is divided by ",num2," is:",rmnd)
+result=(num1+num2)*num3/2#combined arithmetic operations
+print("The result of (",num1,"+",num2,") *",num3,"/2 is:",result)
---
+++ first name.py
@@ -0,0 +1,10 @@
+#Author:Joswin Shaju
+#Date:08/10/2024
+
+str1=input("enter the first name:")
+str2=input("enter the second name:")
+conc=str1+" "+str2
+print(conc)
+l=len(str1)
+first_name=str1[:l]
+print(first_name)
