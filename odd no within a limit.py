6efe8c940076d2bfa68f835d3f182a4525002c64
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-10-15 16:07:23 +0530

    Add files via upload

---
+++ odd no within a limit.py
@@ -0,0 +1,5 @@
+lt=int(input("enter the no limit:"))
+odd_no=1
+while odd_no<=lt:
+    print(odd_no,"\t",end="")
+    odd_no+=2
---
+++ odd nos.py
@@ -0,0 +1,7 @@
+lt=int(input("enter the range:"))
+odd_no=1
+count=0
+while count<lt:
+    print(odd_no,"\t",end="")
+    count+=1
+    odd_no+=2
