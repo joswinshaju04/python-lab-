463585e07768eba68db6b706e1e4514d6cf7f1ff
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-10-15 14:11:10 +0530

    Add files via upload

---
+++ positive or negative int.py
@@ -0,0 +1,10 @@
+#author: Joswin Shaju
+#date:15/10/2024
+
+number=int(input("enter the number:"))
+if number>0:
+    print("the number is positive")
+elif number<0:
+    print("the number is negative")
+else:
+    print("the given number is zero")
