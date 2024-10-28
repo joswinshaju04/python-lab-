29ba91675dc2671ca72adba59e5475429860ab89
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-10-28 22:52:28 +0530

    Update largest of 3.py

--- largest of 3.py
+++ largest of 3.py
@@ -6,7 +6,7 @@ num2=int(input("enter second number:"))
 num3=int(input("enteer third number:"))
 if num1>num2 and num1>num3:
     print("the largest number is:",num1)
-elif num2>num3 and num2>num1:
+elif num2>num3:
     print("the largest number is:",num2)
 else :
     print("the largest number is:",num3)
