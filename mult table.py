d2482d66be000c8491edfb03df918f0a27d5d000
joswinshaju04 <joswinshaju2028@cs.sjcetpalai.ac.in>
2024-10-29 15:22:30 +0530

    Add files via upload

--- mult table.py
+++ mult table.py
@@ -2,7 +2,7 @@
 Author:Joswin Shaju'''
 
 n=int(input("enter the number:"))
-b=int(input("rnter the limit:"))
+b=int(input("enter the limit:"))
 for i in range (1,b+1):
     a=n*i
     print(f" {i} * {n} = {a}")
